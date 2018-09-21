141f338058773756ad23800f9b1281c53adb8e19
Wolfy-J <wolfy.jd@gmail.com>
2018-09-21 15:46:59 +0300

    binlog

--- tests/fix-binlog.sh
+++ tests/fix-binlog.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -ex
-sudo echo "binlog-format = MIXED" >> /etc/mysql/my.cnf
+sed -i '/\[mysqld\]/a\
+binlog_format = MIXED' /etc/mysql/my.cnf
+
 sudo cat /etc/mysql/my.cnf
 sudo service mysql restart
