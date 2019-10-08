8874647368b367eeda50968ecdd240cee0db9e93
Wolfy-J <wolfy.jd@gmail.com>
2019-10-08 16:53:50 +0300

    - fix sqlserver installation

--- tests/install-sqlsrv.sh
+++ tests/install-sqlsrv.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -ex
+sudo apt-get update
 sudo apt install unixodbc unixodbc-dev
+ACCEPT_EULA=Y sudo apt-get install -qy msodbcsql17 mssql-tools libssl1.0.0
 pecl install pdo_sqlsrv-5.6.1
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql.list
