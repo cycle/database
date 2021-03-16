9473f06d751de63156135d41d4ea684daae9d001
roxblnfk <roxblnfk@ya.ru>
2021-03-16 14:16:23 +0300

    Remove redundant code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -20,14 +20,9 @@ mb_internal_encoding('UTF-8');
 //Composer
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-$db = getenv('DB') ?: null;
-
 $drivers = [
     'sqlite'    => [
         'driver'     => Database\Driver\SQLite\SQLiteDriver::class,
-        'check'      => static function () use ($db) {
-            return $db === 'sqlite' || in_array('sqlite', \PDO::getAvailableDrivers(), true);
-        },
         'conn'       => 'sqlite::memory:',
         'user'       => 'sqlite',
         'pass'       => '',
@@ -35,9 +30,6 @@ $drivers = [
     ],
     'mysql'     => [
         'driver'     => Database\Driver\MySQL\MySQLDriver::class,
-        'check'      => static function () use ($db) {
-            return $db === 'mysql' || in_array('mysql', \PDO::getAvailableDrivers(), true);
-        },
         'conn'       => 'mysql:host=127.0.0.1:13306;dbname=spiral',
         'user'       => 'root',
         'pass'       => 'root',
@@ -45,9 +37,6 @@ $drivers = [
     ],
     'postgres'  => [
         'driver'     => Database\Driver\Postgres\PostgresDriver::class,
-        'check'      => static function () use ($db) {
-            return $db === 'postgres' || in_array('pgsql', \PDO::getAvailableDrivers(), true);
-        },
         'conn'       => 'pgsql:host=127.0.0.1;port=15432;dbname=spiral',
         'user'       => 'postgres',
         'pass'       => 'postgres',
@@ -55,9 +44,6 @@ $drivers = [
     ],
     'sqlserver' => [
         'driver'     => Database\Driver\SQLServer\SQLServerDriver::class,
-        'check'      => static function () use ($db) {
-            return $db === 'sqlserver' || in_array('sqlsrv', \PDO::getAvailableDrivers(), true);
-        },
         'conn'       => 'sqlsrv:Server=127.0.0.1,11433;Database=tempdb',
         'user'       => 'SA',
         'pass'       => 'SSpaSS__1',
@@ -65,6 +51,7 @@ $drivers = [
     ],
 ];
 
+$db = getenv('DB') ?: null;
 Database\Tests\BaseTest::$config = [
         'debug' => false,
     ] + ($db === null
