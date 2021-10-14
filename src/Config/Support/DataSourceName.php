2728e0953bb4f7a99bbc515c5a139c292432509d
SerafimArts <nesk@xakep.ru>
2021-10-14 12:15:21 +0300

    Improve read pcre

--- src/Config/Support/DataSourceName.php
+++ src/Config/Support/DataSourceName.php
@@ -39,7 +39,7 @@ final class DataSourceName
     public static function read(string $haystack, array|string $needle): ?string
     {
         $needle = \array_map(static fn(string $item): string => \preg_quote($item), (array)$needle);
-        $pattern = \sprintf('/(?:%s)=([^;]+)/i', \implode('|', $needle));
+        $pattern = \sprintf('/\b(?:%s)=([^;]+)/i', \implode('|', $needle));
 
         if (\preg_match($pattern, $haystack, $matches)) {
             return $matches[1];
