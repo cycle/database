316cc51bb35bb8676a717e75b499d47fb6d24ed7
Maxim Smakouz <m.smakouz@gmail.com>
2023-01-16 17:28:50 +0200

    Add MySQL Set support

--- tests/Database/Functional/Driver/MySQL/Schema/ConsistencyTest.php
+++ tests/Database/Functional/Driver/MySQL/Schema/ConsistencyTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Cycle\Database\Tests\Functional\Driver\MySQL\Schema;
 
 // phpcs:ignore
+use Cycle\Database\Driver\MySQL\Schema\MySQLColumn;
 use Cycle\Database\Tests\Functional\Driver\Common\Schema\ConsistencyTest as CommonClass;
 
 /**
@@ -14,4 +15,20 @@ use Cycle\Database\Tests\Functional\Driver\Common\Schema\ConsistencyTest as Comm
 class ConsistencyTest extends CommonClass
 {
     public const DRIVER = 'mysql';
+
+    public function testSet(): void
+    {
+        $schema = $this->schema('table');
+        $this->assertFalse($schema->exists());
+
+        /** @var MySQLColumn $column */
+        $column = $schema->set('target', ['a', 'b', 'value']);
+
+        $schema->save();
+        $schema = $this->schema('table');
+        $this->assertTrue($schema->exists());
+        $this->assertTrue($schema->column('target')->compare($column));
+        $this->assertSame(['a', 'b', 'value'], $column->getEnumValues());
+        $this->assertSame(['a', 'b', 'value'], $schema->column('target')->getEnumValues());
+    }
 }
