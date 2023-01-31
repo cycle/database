c08a927e93c1ed62d074dbf3600a8aec8263fb9e
Maxim Smakouz <m.smakouz@gmail.com>
2023-01-31 14:59:09 +0200

    Interpolator optimization

--- tests/Database/Unit/Query/InterpolatorTest.php
+++ tests/Database/Unit/Query/InterpolatorTest.php
@@ -313,4 +313,39 @@ class InterpolatorTest extends TestCase
             $interpolated
         );
     }
+
+    public function testLargeInterpolation(): void
+    {
+        $query = null;
+        $parameters = null;
+        $expected = null;
+
+        include \dirname(__DIR__) . '/Stub/InterpolatorTestLargeFixture.php';
+
+        if (!\is_string($query) || !\is_array($parameters) || !\is_string($expected)) {
+            $this->markTestSkipped('no test data');
+        }
+
+        $interpolated = Interpolator::interpolate($query, $parameters);
+
+        $this->assertSame($expected, $interpolated);
+    }
+
+    public function testNoRecursiveInterpolation(): void
+    {
+        $query = 'SELECT * FROM table WHERE name = :name AND str = ? AND number = :number';
+
+        $parameters = [
+            ':name' => 'Hello?',
+            ':number',
+            ':number' => 42,
+        ];
+
+        $interpolated = Interpolator::interpolate($query, $parameters);
+
+        $this->assertSame(
+            'SELECT * FROM table WHERE name = \'Hello?\' AND str = \':number\' AND number = 42',
+            $interpolated
+        );
+    }
 }
