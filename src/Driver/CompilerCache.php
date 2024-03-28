3564e946cce53256e8a20dca6be4670a52e58468
Maxim Smakouz <m.smakouz@gmail.com>
2024-03-28 13:20:08 +0200

    Fix stringable checking

--- src/Driver/CompilerCache.php
+++ src/Driver/CompilerCache.php
@@ -121,8 +121,10 @@ final class CompilerCache implements CompilerInterface
 
             if ($value->isArray()) {
                 foreach ($value->getValue() as $child) {
-                    if ($child instanceof FragmentInterface && $child instanceof \Stringable) {
-                        $hash .= '_F_' . $child;
+                    if ($child instanceof FragmentInterface) {
+                        if ($child instanceof \Stringable) {
+                            $hash .= '_F_'. $child;
+                        }
                         continue;
                     }
 
