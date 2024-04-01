6b6191cc3342f0bda40d6fb49e901257cf826c13
Maxim Smakouz <m.smakouz@gmail.com>
2024-04-01 17:08:55 +0300

    Add unit test

---
+++ tests/Database/Unit/Driver/CompilerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\Database\Tests\Unit\Driver;
+
+use Cycle\Database\Driver\Compiler;
+use Cycle\Database\Driver\Quoter;
+use Cycle\Database\Query\QueryParameters;
+use PHPUnit\Framework\TestCase;
+
+final class CompilerTest extends TestCase
+{
+    public function testCompileJsonOrderByShouldReturnOriginalStatement(): void
+    {
+        $compiler = new class extends Compiler {
+            protected function limit(QueryParameters $params, Quoter $q, int $limit = null, int $offset = null): string
+            {
+            }
+        };
+
+        $ref = new \ReflectionMethod($compiler, 'compileJsonOrderBy');
+        $ref->setAccessible(true);
+
+        $this->assertSame('foo-bar', $ref->invoke($compiler, 'foo-bar'));
+    }
+}
