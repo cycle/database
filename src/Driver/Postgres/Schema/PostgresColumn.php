4feb554a50f09815bbd86da84807d04f2d76b17d
roxblnfk <roxblnfk@ya.ru>
2024-10-22 18:49:44 +0400

    chore: simplify code; update dependencies

--- src/Driver/Postgres/Schema/PostgresColumn.php
+++ src/Driver/Postgres/Schema/PostgresColumn.php
@@ -523,9 +523,9 @@ class PostgresColumn extends AbstractColumn
             $schema['typname'] === 'timestamp' || $schema['typname'] === 'timestamptz' => 'timestamp',
             $schema['typname'] === 'date' => 'date',
             $schema['typname'] === 'time' || $schema['typname'] === 'timetz' => 'time',
-            $schema['typname'] === '_varchar' || $schema['typname'] === '_text' || $schema['typname'] === '_bpchar'  => 'string[]',
-            mb_substr($schema['typname'],0,4) === '_int' => 'integer[]',
-            $schema['typname'] === '_numeric' || mb_substr($schema['typname'],0,6) === '_float' => 'float[]',
+            \in_array($schema['typname'], ['_varchar', '_text', '_bpchar'], true) => 'string[]',
+            \str_starts_with($schema['typname'], '_int') => 'integer[]',
+            $schema['typname'] === '_numeric', \str_starts_with($schema['typname'], '_float') => 'float[]',
             default => $schema['data_type']
         };
 
