f60ac58b2d843c5162c3fe839321a0336aa24ba1
roxblnfk <roxblnfk@ya.ru>
2026-02-16 15:22:47 +0400

    chore: update deprecation notice for MYSQL_ATTR_INIT_COMMAND in ConnectionConfig

--- src/Config/MySQL/ConnectionConfig.php
+++ src/Config/MySQL/ConnectionConfig.php
@@ -27,9 +27,8 @@ abstract class ConnectionConfig extends BaseConnectionConfig
         \PDO::ATTR_CASE => \PDO::CASE_NATURAL,
         \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
         /**
-         * @see PDO::MYSQL_ATTR_INIT_COMMAND
-         * @see \Pdo\Mysql::ATTR_INIT_COMMAND
-         *
+         * @see \PDO::MYSQL_ATTR_INIT_COMMAND is deprecated since PHP 8.5
+         * @todo Replace with {@see \Pdo\Mysql::ATTR_INIT_COMMAND} when minimum PHP version is 8.5
          * @todo Should be moved into common driver settings?
          */
         1002 => 'SET NAMES "UTF8"',
